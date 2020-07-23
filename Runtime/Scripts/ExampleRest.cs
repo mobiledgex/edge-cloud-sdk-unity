b8874f73966299e9b9fca014c818ffd4ed10b595
Ahmed Schrute <ahmedhussam.hellomobile@gmail.com>
2020-07-22 18:45:58 -0700

    DMEDNS handling

---
+++ Runtime/Scripts/ExampleRest.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking; // for Unity Web Request
+
+using MobiledgeX;
+using DistributedMatchEngine;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System;
+
+    [RequireComponent(typeof(MobiledgeX.LocationService))]
+    public class ExampleRest : MonoBehaviour
+    {
+        MobiledgeXIntegration mxi;
+
+        IEnumerator Start()
+        {
+            yield return StartCoroutine(MobiledgeX.LocationService.EnsureLocation());
+            GetEdgeConnection();
+        }
+
+        async void GetEdgeConnection()
+        {
+            mxi = new MobiledgeXIntegration();
+            try
+            {
+                await mxi.RegisterAndFindCloudlet();
+            }
+            catch (DmeDnsException)
+            {
+                mxi.UseWifiOnly(true);
+                await mxi.RegisterAndFindCloudlet();
+            }
+            mxi.GetAppPort(LProto.L_PROTO_HTTP);
+            string url = mxi.GetUrl("http");
+            Debug.Log("Rest URL is : " + url);
+
+            StartCoroutine(RestExample(url)); // You can use RestExampleHttpClient
+                                              //await RestExampleHttpClient(url);
+        }
+
+        IEnumerator RestExample(string url)
+        {
+            UnityWebRequest www = UnityWebRequest.Get(url);
+            yield return www.SendWebRequest();
+
+            if (www.isHttpError || www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+
+        async Task<HttpResponseMessage> RestExampleHttpClient(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(url);
+            return await httpClient.GetAsync("/"); //makes a get request
+        }
+    }
+
---
+++ Runtime/Scripts/ExampleWebSocket.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+using MobiledgeX;
+using DistributedMatchEngine;
+
+
+    [RequireComponent(typeof(MobiledgeX.LocationService))]
+    public class ExampleWebSocket : MonoBehaviour
+    {
+        MobiledgeXWebSocketClient wsClient;
+        MobiledgeXIntegration mxi;
+
+        IEnumerator Start()
+        {
+            yield return StartCoroutine(MobiledgeX.LocationService.EnsureLocation());
+            GetEdgeConnection();
+        }
+
+        async void GetEdgeConnection()
+        {
+            mxi = new MobiledgeXIntegration();
+            try
+            {
+                await mxi.RegisterAndFindCloudlet();
+            }
+            catch (DmeDnsException)
+            {
+                mxi.UseWifiOnly(true);
+                await mxi.RegisterAndFindCloudlet();
+            }
+            
+            mxi.GetAppPort(LProto.L_PROTO_TCP);
+            string url = mxi.GetUrl("ws");
+            Debug.Log("WebSocket URL is : " + url);
+            await StartWebSocket(url);
+            //wsClient.Send("WebSocketMsg");// You can send  Text or Binary messages to the WebSocket Server 
+        }
+
+
+        async Task StartWebSocket(string url)
+        {
+            wsClient = new MobiledgeXWebSocketClient();
+            if (wsClient.isOpen())
+            {
+                wsClient.Dispose();
+                wsClient = new MobiledgeXWebSocketClient();
+            }
+
+            Uri uri = new Uri(url);
+            await wsClient.Connect(uri);
+        }
+
+
+
+        // Dequeue WebSocket Messages every frame (if there is any)
+        private void Update()
+        {
+            if (wsClient == null)
+            {
+                return;
+            }
+            var cqueue = wsClient.receiveQueue;
+            string msg;
+            while (cqueue.TryPeek(out msg))
+            {
+                cqueue.TryDequeue(out msg);
+                Debug.Log("WebSocket Received messgae : " + msg);
+            }
+        }
+
+    }
+
