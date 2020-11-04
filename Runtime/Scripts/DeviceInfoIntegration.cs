c1eee631d62747252c339e6519f07d5cfac8ce9a
franklin-huang-mobiledgex <franklin.huang@mobiledgex.com>
2020-11-04 11:44:49 -0600

    add GetDeviceInfo to DeviceInfo implementation

--- Runtime/Scripts/DeviceInfoIntegration.cs
+++ Runtime/Scripts/DeviceInfoIntegration.cs
@@ -132,14 +132,23 @@ namespace MobiledgeX
       return map;
     }
 #elif UNITY_IOS
+  [DllImport("__Internal")]
+  private static extern Dictionary<string, string> _getDeviceInfo();
+
   public Dictionary<string, string> GetDeviceInfo()
   {
-    Debug.LogFormat("[{0}] DeviceInfo not implemented!", TAG);
+    Dictionary<string, string> deviceInfo = new Dictionary<string, string>();
+    if (Application.platform == RuntimePlatform.IPhonePlayer)
+    {
+      deviceInfo = _getDeviceInfo());
+    }
+    return deviceInfo;
   }
 #else // Unsupported platform.
   public Dictionary<string, string> GetDeviceInfo()
   {
-    Debug.LogFormat("[{0}] DeviceInfo not implemented!", TAG);
+    Debug.LogFormat("DeviceInfo not implemented!");
+    return null;
   }
 #endif
   }
