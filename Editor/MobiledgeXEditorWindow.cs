08609bcc45ac3495a36557ac54a4341fe0e98b44
lgarner <1672745+lgarner@users.noreply.github.com>
2021-03-01 14:48:18 -0800

    Copyright 2021 (#130)

--- Editor/MobiledgeXEditorWindow.cs
+++ Editor/MobiledgeXEditorWindow.cs
@@ -312,7 +312,7 @@ namespace MobiledgeX
         private void LicenseWindow()
         {
             EditorGUILayout.BeginHorizontal();
-            string licenseText = "Copyright 2020 MobiledgeX, Inc.All rights and licenses reserved.\n MobiledgeX, Inc. 156 2nd Street #408, San Francisco, CA 94105" +
+            string licenseText = "Copyright 2021 MobiledgeX, Inc.All rights and licenses reserved.\n MobiledgeX, Inc. 156 2nd Street #408, San Francisco, CA 94105" +
             "Licensed under the Apache License, Version 2.0 (the \"License\") \n you may not use this file except in compliance with the License.\n You may obtain a copy of the License at" +
             "\n \n  http://www.apache.org/licenses/LICENSE-2.0  \n  \n Unless required by applicable law or agreed to in writing, software \n distributed under the License is distributed on an \"AS IS- BASIS\" \n" +
             "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. \n See the License for the specific language governing permissions and\n limitations under the License.";
